8f933948a1d93ff2cac1bc09f048b96f031ef88f
tribunalfederal <matteusgsilva@gmail.com>
2025-09-06 06:11:45 -0300

    Create Player.js file in src/game/

---
+++ src/game/Player.js
@@ -0,0 +1,57 @@
+class Player {
+    constructor() {
+        this.profile = {
+            name: "",
+            artistName: "",
+            age: 18,
+            appearance: {
+                height: 170,
+                hairStyle: "",
+                hairColor: "",
+                skinColor: "",
+                clothingStyle: ""
+            },
+            stats: {
+                singing: 1,
+                performance: 1,
+                charisma: 1,
+                songwriting: 1,
+                production: 1
+            },
+            career: {
+                level: 1,
+                experience: 0,
+                fans: 0,
+                monthlyListeners: 0,
+                reputation: 0
+            },
+            finances: {
+                balance: 1000,
+                income: {
+                    streaming: 0,
+                    shows: 0,
+                    merch: 0
+                },
+                expenses: {
+                    marketing: 0,
+                    production: 0,
+                    living: 0
+                }
+            }
+        };
+    }
+
+    updateStats(newStats) {
+        this.profile.stats = { ...this.profile.stats, ...newStats };
+    }
+
+    updateCareer(careerUpdate) {
+        this.profile.career = { ...this.profile.career, ...careerUpdate };
+    }
+
+    updateFinances(transaction) {
+        this.profile.finances.balance += transaction.amount;
+    }
+}
+
+export default Player;
